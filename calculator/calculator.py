088916be57711200f41496d628d0160abeb5515d
Joshua Hecker <jhecker2001@gmail.com>
2023-10-18 09:05:30 -0500

    Created functions for the various operations of the calculator.

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,3 +1,20 @@
 import art
 
 print(art.logo)
+
+def add(n1, n2):
+    """Adds two numbers together."""
+    return n1+n2
+
+def subtract(n1, n2):
+    """Subtracts a number from another number."""
+    return n1-n2
+
+def multiply(n1, n2):
+    """Multiplies two numbers."""
+    return n1*n2
+
+def divide(n1, n2):
+    """Divides a number by another number."""
+    return n1/n2
+
